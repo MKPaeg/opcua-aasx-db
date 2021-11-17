741b470372c29653f1c6efbdb716a61b99384ef2
Yanghojun <hojune0303@naver.com>
2021-11-17 16:38:28 +0900

    bug fix

--- mariadb.py
+++ mariadb.py
@@ -26,7 +26,6 @@ class MariaDB:
         
         for table_name, column_names in self.table_dict['db_info'].items():
             if table_name not in self.args.table_list:
-            # if table_name in "DEPTHIMAGE" or table_name == "HSIIMAGE":
                 continue
             
             for cl_sql in column_names:
@@ -34,21 +33,12 @@ class MariaDB:
                 
             sql = f"CREATE TABLE IF NOT EXISTS {table_name}" + f"({date_time_sql}, {temp_sql})"
             sql = sql[:-3] + ")"        # postprocess for last column
-            print(sql)
+            print(f"{table_name} created..")
             self.execute_sql(sql)
         
-        # sql = f"CREATE TABLE IF NOT EXISTS DEPTHIMAGE(ts DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, DEPTH_WIDTH DECIMAL(10,2), DEPTH_HEIGHT DECIMAL(10,2), DEPTH_AVG_WIDTH DECIMAL(10,2))"
-        # self.execute_sql(sql)
-        
-        # sql = f"CREATE TABLE IF NOT EXISTS HSIIMAGE(ts DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, HSI_CROP_WIDTH DECIMAL(10,2), HSI_CROP_HEIGHT DECIMAL(10,2), HSI_LABEL_BAND_NUM DECIMAL(10,2), HSI_INTERLEAVE DECIMAL(10,2), HSI_DATA_TYPE DECIMAL(10,2), HSI_BANDS DECIMAL(10,2), HSI_DATASIZE DECIMAL(10,2), HSI_BAND_STRIDE DECIMAL(10,2))"
-        # self.execute_sql(sql)
-        
-        # sql = f"CREATE TABLE IF NOT EXISTS RESULT(ts DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, RESULT_TOMATO_RIPENESS DECIMAL(10,2), RESULT_TOMATO_SP DECIMAL(10,2))"
-        # self.execute_sql(sql)
-        
     def delete_table(self):
-        for table_name in self.table_dict['db_info'].keys():
-            if table_name == "DEPTHIMAGE" or table_name == "HSIIMAGE":
+        for table_name, column_names in self.table_dict['db_info'].items():
+            if table_name not in self.args.table_list:
                 continue
             
             sql = f"DROP TABLE {table_name}"
